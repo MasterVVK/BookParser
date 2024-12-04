8b65d5a045fdbb0551e3f2fa7fa8b2f292c36bd6
MasterVVK <vvk@2140101.ru>
2024-12-04 10:52:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from database.models import session, Book
 if __name__ == '__main__':
     start_url = 'https://wuxiaworld.ru/imperator-syanvu/kontrataka-izgnannogo-uchenika-glava-1-uchenik-sekty/'
     book_title = 'Контратака изгнанного ученика'
-    max_chapters = 500  # Указываем максимальное количество глав
+    max_chapters = 3365  # Указываем максимальное количество глав
 
     # Список текстов, которые нужно исключить при парсинге
     excluded_texts = [
